426947d8f6c05dee945cfd1ee40de4397943578b
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-06-21 21:53:09 -0300

    test: :test_tube: error page test

---
+++ web/src/pages/error/error.test.tsx
@@ -0,0 +1,17 @@
+import "@testing-library/jest-dom";
+import {render} from "@testing-library/react";
+import {describe, expect, it}  from "vitest";
+import {Error} from "./";
+import { MemoryRouter } from 'react-router-dom';
+describe("Error", () => {
+    it("should like error page is render", ()=> {
+        const {getByText} = render(
+        <MemoryRouter>
+            <Error />
+        </MemoryRouter>
+        );
+
+        expect(getByText("We can't find that page.")).toBeInTheDocument();
+        expect(getByText('Home')).toBeInTheDocument();
+    })
+})
