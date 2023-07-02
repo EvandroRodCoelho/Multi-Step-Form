f790443d3feb08249de2f37107a790ecf64988fe
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-07-02 00:09:30 -0300

    feat: :sparkles: Create user Schema

--- server/src/schemas/createUserSchema.ts
+++ server/src/schemas/createUserSchema.ts
@@ -1,16 +1,27 @@
 import { z } from "zod";
 
-export const createUserSchema = z.object({
-    fullName: z.string().nonempty("FullName is required")
-    .regex(/^[a-zA-Z\s]*$/, "Cannot contain special characters or numbers"),
-    email: z.string().nonempty("Email is required").email(),
-    zipCode:z.string().nonempty("Zip Code is required"), 
-    city:z.string().nonempty("City is required"),
-    country:z.string().nonempty("Country is required"),
-    state:z.string().nonempty("State is required"),
-    gender:z.string().nonempty("Gender is required"),
-    password: z.string().nonempty("Password is required").min(6,"Password must be at least 6 characters"),
-    urlLinkedin:z.string().nonempty("Linkedin url is required").url("Is not url link"),
-    urlGitHub:z.string().nonempty("GitHub url is required").url("Is not url link"),
-})
+const addressSchema = z.object({
+  city: z.string().nonempty("City is required"),
+  country: z.string().nonempty("Country is required"),
+  state: z.string().nonempty("State is required"),
+  zipCode: z.string().nonempty("Zip Code is required"),
+});
 
+const socialProfileSchema = z.object({
+  urlGitHub: z.string().nonempty("GitHub url is required").url("Is not a valid URL"),
+  urlLinkedin: z.string().nonempty("LinkedIn url is required").url("Is not a valid URL"),
+});
+
+const informationPessoalSchema =  z.object({
+    email: z.string().nonempty("Email is required").email(),
+    fullName: z.string()
+      .nonempty("Full Name is required")
+      .regex(/^[a-zA-Z\s]*$/, "Cannot contain special characters or numbers"),
+    password: z.string().nonempty("Password is required"),
+    gender: z.string().nonempty("Gender is required"),
+  })
+export const createUserSchema = z.object({
+  informationPessoal:informationPessoalSchema,
+  address: addressSchema,
+  socialProfile: socialProfileSchema,
+});
