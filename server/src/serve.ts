f8f8beae8923df4facadf41044bc4d550ca32fc9
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-07-02 00:12:46 -0300

    feat: :sparkles: Validade Routes

--- server/src/serve.ts
+++ server/src/serve.ts
@@ -5,7 +5,9 @@ import { loginUserSchema } from "./schemas/loginUserSchema.";
 import fastifyCors  from "@fastify/cors";
 import { emailSchema } from "./schemas/emailSchema";
 
-const app = fastify();
+const app = fastify({
+  logger: false
+});
 
 app.register(fastifyCors, {
     origin: true,
@@ -21,41 +23,36 @@ app.get('/users', async () => {
 })
 
 app.post('/register/create', async (req, res) => {
-
-    try {
-        const  { city, country, email, fullName, 
-            gender, urlGitHub,urlLinkedin,zipCode,state,password } = createUserSchema.parse(req.body);
-                  
-        const existingUser = await prisma.user.findUnique({
-            where: { email },
-        });
-
-        if(existingUser) {
-            return res.status(409).send("Email already exists");
-        }
-        await prisma.user.create({
-            data: {
-                city,
-                country,
-                email,
-                fullName,
-                gender,
-                password,
-                urlGitHub,
-                urlLinkedin,
-                zipCode,
-                state,
-            }})
+  try {
+    const userData = createUserSchema.parse(req.body);
   
-
-            return res.status(201).send();
-    }
-   catch (err) {
-        console.error(err);
-        return res.status(500).send("Error creating user");
-    }
-
-})
+    const { informationPessoal, address, socialProfile } = userData;
+  
+    const { email, fullName, password, gender } = informationPessoal;
+    const { city, country, state, zipCode } = address;
+    const { urlGitHub, urlLinkedin } = socialProfile;
+  
+    await prisma.user.create({
+      data: {
+        city,
+        country,
+        email,
+        fullName,
+        gender,
+        password,
+        urlGitHub,
+        urlLinkedin,
+        zipCode,
+        state,
+      },
+    });
+  
+    return res.status(201).send();
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Error creating user");
+  }
+});
 
 app.post("/login", async (req, res) => {
     try {
@@ -98,14 +95,20 @@ app.post("/register/step1", async (req, res) => {
   try {
     const { email } = emailSchema.parse(req.body);
 
-    const user = await prisma.user.findUnique({ where: { email } });
-  
-    if (user) {
-      return res.status(404).send({ error: "Email is exist" });
+    const user = await prisma.user.findFirst({ where: { email } });
+    if(user) {
+      return res.status(401).send({ error: "Email is exist" });
     }
-    return res.send({ message: "Registration step 1 successful" });
+    
+    prisma.$disconnect;
+    
   }
   catch (err) {
+    if(res.statusCode === 404) {
+      prisma.$disconnect;
+      return res.status(200).send("login");
+    }
+    prisma.$disconnect;
     return res.status(500).send("Error logging in");
   }
 
